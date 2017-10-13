eb07edfdc0a6041898b2219b5295e7f11bda3c8d
jjYukgm <andykgm39@gmail.com>
2017-10-13 19:50:20 +0800

    I cannot make sure the version is syncgit status

---
+++ analysis.py
@@ -0,0 +1,120 @@
+
+import os
+import argparse
+
+import pdb
+# pdb.set_trace = lambda: None
+import scipy.io as sio
+import zipfile
+import numpy as np
+import shutil
+
+# plot
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+# pr curve
+from sklearn import preprocessing
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+'''
+python analysis.py -n r_37_2_lr1e-1_e200,r_37_2_lr1e-1_best,r_37_2_lr1e-2_e200,r_37_2_lr1e-2_best --prc 
+'''
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 analysis')
+parser.add_argument('-n', '--net', default="net", type=str, help='net name')
+parser.add_argument('--debug', '-d', action='store_true', help='debug mode')
+parser.add_argument('--prc', action='store_true', help='prec recall curve')
+parser.add_argument('--gr', action='store_true', help='plot group')
+args = parser.parse_args()
+
+
+def pr_curves(nets, train=False):
+    nets = nets.split(",")
+    for n in nets:
+        pr_curve(n)
+
+def pr_curve(net,train=False):
+    # load pred
+    if train:
+        fn = "train"
+    else:
+        fn = "test"
+    # get data & analysis ap, recall
+    print(net+'_'+fn+": (recall, prec)")
+    mat = sio.loadmat(os.path.join('mat', net, fn+'.mat'))
+    y_score = np.array(mat['pred_confidence'])
+    num_cls = y_score.shape[-1]
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(range(0, num_cls))
+    gt_bi = lb.transform(np.array(mat['ground_truth']).squeeze())
+    if num_cls==2:
+        gt_bi = np.hstack((gt_bi, 1 - gt_bi))
+    
+    # For each class
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(num_cls):
+        precision[i], recall[i], _ = precision_recall_curve(gt_bi[:, i], y_score[:, i])
+        average_precision[i] = average_precision_score(gt_bi[:, i], y_score[:, i])
+
+    # A "micro-average": quantifying score on all classes jointly
+    precision["micro"], recall["micro"], _ = precision_recall_curve(gt_bi.ravel(), y_score.ravel())
+    average_precision["micro"] = average_precision_score(gt_bi, y_score, average="micro")
+    print('Average precision score, micro-averaged over all classes: {0:0.2f}'.format(average_precision["micro"]))
+    # plot
+    lines = []
+    labels = []
+
+    for i in range(num_cls):
+        l, = plt.plot(recall[i], precision[i], lw=2)
+        lines.append(l)
+        labels.append('class {0} (area = {1:0.2f})'
+                      ''.format(i, average_precision[i]))
+
+    l, = plt.plot(recall["micro"], precision["micro"], color='gold', lw=2)
+    lines.append(l)
+    labels.append('micro-average Precision-recall (area = {0:0.2f})'
+                  ''.format(average_precision["micro"]))
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.75)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall curve to multi-class')
+    lgd = plt.legend(lines, labels, loc=(0, -1.0), prop=dict(size=14))
+    # save
+    sdir = os.path.join('plot',net)
+    if not os.path.isdir(sdir):
+        os.makedirs(sdir)
+    plt.tight_layout()
+    plt.savefig(os.path.join(sdir,"plot_"+fn+"_prc.png"), bbox_extra_artists=(lgd,), bbox_inches='tight', dpi=fig.dpi)
+    plt.close(fig)
+ 
+def remove_file():
+    if args.net.find(',') > -1:
+        nets = nets.split(",")
+        for n in nets:
+            shutil.rmtree(os.path.join('plot', n), ignore_errors=True)
+    else:
+        shutil.rmtree(os.path.join('plot', args.net), ignore_errors=True)
+
+plt.ioff() 
+# confirm dir exist
+if not os.path.isdir('plot'):
+    os.mkdir('plot')
+if args.debug:
+    # debug use
+    pdb.set_trace()
+    pr_curve(args.net)
+    # pass 
+if args.prc:
+    if args.net.find(',') > -1:
+        pr_curves(args.net)
+        pr_curves(args.net, train=True)
+    else:
+        pr_curve(args.net)
+        pr_curve(args.net, train=True)
---
+++ main.py
@@ -0,0 +1,387 @@
+'''Train CIFAR10 with PyTorch.'''
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from models import *
+from utils import progress_bar
+from torch.autograd import Variable
+
+import pdb
+# pdb.set_trace = lambda: None
+import scipy.io as sio
+import zipfile
+import numpy as np
+import time
+import shutil
+'''
+python main.py -n r_37
+python main.py -n r_37 -t --p2l &&\
+python main.py -n r_37_2 --dn r_37 --lr 0.01 &&\
+python main.py -n r_37_2_lr1e-1 -t --dn r_37 -c e200 && \
+python main.py -n r_37_2_lr1e-1 -t --dn r_37 && \
+python main.py -n r_37_2_lr1e-2 -t --dn r_37 -c e200 && \
+python main.py -n r_37_2_lr1e-2 -t --dn r_37
+python main.py -n r_37_2 --r2 --dn r_37 -c e200
+
+matlab:
+matlab -nosplash -nodesktop
+    $ load('fn.mat')
+    load('mat/r_37_n_e200/test.mat')
+    who $ to watch variable
+'''
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('-n', '--net', default="net", type=str, help='net name')
+parser.add_argument('--ckptn', '-c', default="best", type=str, help='ckpt name')
+parser.add_argument('--dn', default="", type=str, help='data name')
+parser.add_argument('--pn', default="", type=str, help='pretrain name')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--epoch', '-e', default=200, type=int, help='next training epoch')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--test', '-t', action='store_true', help='test checkpoint')
+parser.add_argument('--zipf', '-z', action='store_true', help='zip the test')
+parser.add_argument('--debug', '-d', action='store_true', help='debug mode')
+parser.add_argument('--p2l', action='store_true', help='pred mat to lbl')
+parser.add_argument('--r2', action='store_true', help='resume to 2 lbl')
+parser.add_argument('--ft', action='store_true', help='finetune only last')
+parser.add_argument('--rm', action='store_true', help='remove net')
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+if args.dn =="":
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+else:
+    trainset = torch.load('./data/'+args.dn+'_2b.train')
+    testset = torch.load('./data/'+args.dn+'_2b.test')
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+# save dir
+net_dir = args.net
+if args.r2:
+    net_dir += '_r2'
+if args.ft:
+    net_dir += '_ft'
+if not args.lr == 0.1:
+    net_dir += "_lr%.0E"%(args.lr)
+    
+if not os.path.isdir('checkpoint/'+net_dir):
+    os.makedirs('checkpoint/'+net_dir)
+
+# Model
+if args.r2:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint to 2 lbl..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    if args.pn=="":
+        checkpoint = torch.load('./checkpoint/'+args.dn+'/'+args.ckptn+'_ckpt.t7')
+    else:
+        checkpoint = torch.load('./checkpoint/'+args.pn+'/'+args.ckptn+'_ckpt.t7')
+    net = checkpoint['net']
+    start_epoch = checkpoint['epoch']
+    # replace para
+    if args.net =="r_37_2":
+        net.linear = nn.Linear(4096, 2)
+    
+    if args.ft:
+        for param in net.parameters():
+            param.requires_grad = False
+        for param in net.linear.parameters():
+            param.requires_grad = True
+    
+if args.resume or args.test:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/'+net_dir+'/'+args.ckptn+'_ckpt.t7')
+    net = checkpoint['net']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+else:
+    print('==> Building model..')
+    # net = VGG('VGG19')
+    # net = ResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    # net = DPN92()
+    # net = ShuffleNetG2()
+    # net = SENet18()
+    if args.net == "r_37":
+        net = r_37()
+    elif args.net =="r_37_2":
+        net = r_37_2()
+    elif args.net =="r_73":
+        net = r_73()
+    elif args.net =="r_110":
+        net = r_110()
+    else:
+        print('There is no ' + args.net)
+        exit()
+    
+if use_cuda:
+    net.cuda()
+    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    cudnn.benchmark = True
+
+
+if args.dn =="":
+    criterion = nn.CrossEntropyLoss()
+else:
+    weights = torch.from_numpy(trainset.weights)
+    weights = weights.type(torch.FloatTensor)
+    if use_cuda:
+        weights = weights.cuda()
+    criterion = nn.CrossEntropyLoss(weight=weights)
+    
+# optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+# branchy case:optim=Adam, lr=0.1, momentum=0.9, weight_decay=0.0001, alpha=0.001
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+
+    
+    
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        optimizer.zero_grad()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        # pdb.set_trace()
+
+        test_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.module if use_cuda else net,
+            'acc': acc,
+            'epoch': epoch+1,
+        }
+        torch.save(state, './checkpoint/'+net_dir+'/best_ckpt.t7')
+        best_acc = acc
+    if (epoch+1) % 10 == 0:
+        print('Save per 10 epoch')
+        state = {
+            'net': net.module if use_cuda else net,
+            'acc': acc,
+            'epoch': epoch+1,
+        }
+        torch.save(state, './checkpoint/'+net_dir+"/e%03d"%(epoch+1)+'_ckpt.t7')
+
+
+def pred2lbl(train=False):
+    if train:
+        fn = "train"
+    else:
+        fn = "test"
+    # load pred
+    matpath = os.path.join('mat', net_dir+'_'+args.ckptn, fn+'.mat')
+    mat = sio.loadmat(matpath)
+    # change to lbl
+    pr = mat['predict_right']   # right: 1; wrong: 0
+    pr = np.squeeze(pr)
+    # weight cls
+    weights = []
+    for i in range(min(pr), max(pr)+1):
+        weights.append((len(pr)+0.)/sum(pr==i))
+    
+    weights = np.array(weights)
+    weights = weights * len(weights)/ sum(weights)  # 0918: nor sigma = num of cls
+    
+    pr = pr.tolist()
+    # conbime with data
+    if train:
+        dataset = trainset
+        dataset.train_labels = pr
+    else:
+        dataset = testset
+        dataset.test_labels = pr
+    dataset.weights = weights
+    # save net datasets
+    torch.save(dataset, './data/'+args.net+'_2b.'+fn)
+    
+def data_save(train=False):
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    runtime = 0.
+    if train:
+        loader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=False, num_workers=2)
+        fn = "train"
+    else:
+        loader = testloader
+        fn = "test"
+    for batch_idx, (inputs, targets) in enumerate(loader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        last_time = time.time()
+        begin_time = last_time
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+
+        _, predicted = torch.max(outputs.data, 1)
+        last_time = time.time()
+        test_loss += loss.data[0]
+        total += targets.size(0)
+        pr = predicted.eq(targets.data).cpu()
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        # save data:
+        out = outputs.data.cpu().numpy()   # [ 100, 10] no SM
+        pred = predicted.cpu().numpy()  # [ 100, ] predict 
+        gt = targets.data.cpu().numpy() # [ 100, ] gt
+        runtime += last_time - begin_time   # time(b=100)(sec)
+        if batch_idx==0:
+            dist = out
+            preds = pred
+            gts = gt
+        else:
+            dist = np.concatenate((dist, out), axis=0)
+            preds = np.concatenate((preds, pred), axis=0)
+            gts = np.concatenate((gts, gt), axis=0)
+    # Save checkpoint.
+    acc = 100.*correct/total
+    prbool = np.equal(preds, gts)
+    pright = np.ones(preds.shape, dtype=np.int8)
+    pright[prbool==False] = 0.
+    print('Saving data..')
+    dists = np.sort(dist, axis=-1)
+    help_str = "pred_confidence: distribution of predicted confidence, array[n, d] \n n: num of data, d: num of class \n *_sort: sorted by d(small to large) \n class_predict: predict class[0:9] \n predict_right: predict right/wrong[1,0] \n accuracy: total accuracy[0,100] \n runtime: runtime on n data in seconds \n epoch: training epoch[best accu] "
+    state = {
+        'epoch': start_epoch,
+        'pred_confidence': dist, 
+        'pred_confidence_sort': dists, 
+        'ground_truth': gts, 
+        'class_predict': preds, 
+        'predict_right': pright, 
+        'accuracy': acc, 
+        'runtime': runtime,
+        'help_str':help_str,
+    }
+    sdir = os.path.join('mat', net_dir+'_'+args.ckptn)
+    if not os.path.isdir(sdir):
+        os.makedirs(sdir)
+    sio.savemat(sdir+'/'+fn+'.mat', state, do_compression=True)
+
+def get_zip():
+    zf = zipfile.ZipFile('./mat/'+net_dir +'_'+args.ckptn+ '.zip', mode='w', compression = zipfile.ZIP_DEFLATED)
+    for root, folders, files in os.walk("./mat"):
+        if root.find(net_dir+'_'+args.ckptn) > -1:
+            print("root: " + root)
+            for sfile in files:
+                if sfile.find('.mat') > -1:
+                    aFile = os.path.join(root, sfile)
+                    #   print aFile
+                    zf.write(aFile)
+
+def remove_file():
+    shutil.rmtree('./checkpoint/'+net_dir, ignore_errors=True)
+    shutil.rmtree('./mat/'+net_dir+'_'+args.ckptn, ignore_errors=True)
+    try:
+        os.remove('./mat/'+net_dir+'_'+args.ckptn+'.zip')
+        os.remove('./data/'+args.net+'_2b.train')
+        os.remove('./data/'+args.net+'_2b.test')
+    except: 
+        print("There is not all data exist!")
+
+if args.debug:
+    pdb.set_trace()
+    get_zip()
+    # test(10)
+    # pred2lbl()
+    # pred2lbl(train=True)
+elif args.rm:
+    remove_file()
+elif args.zipf:
+    get_zip()
+elif args.test:
+    data_save()
+    data_save(train=True)
+    get_zip()
+    print("load epoch: "+ str(start_epoch))
+else:
+    for epoch in range(start_epoch, start_epoch+args.epoch):
+        train(epoch)
+        test(epoch)
+
+if args.p2l:
+    pred2lbl()
+    pred2lbl(train=True)
